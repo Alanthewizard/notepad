b7d9570254e521306b10dfee63f2a9d80ae69520
Alanthewizard <alanzxz@gmail.com>
2024-08-25 06:10:10 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Notes from './components/Notes';
+import NotesForm from './components/NotesForm';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+}from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Notes/> ,
+  },
+  {path:"/notesform",
+    element:<NotesForm/> ,
+
+  }
+
+
+
+
+])
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}/>
     </div>
   );
 }
